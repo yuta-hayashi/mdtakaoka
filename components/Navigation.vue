ae96fe5864c1e27b4d734155c5ab58c40cc23415
yuta-hayashi <dev.hyuta@gmail.com>
2019-06-27 10:03:04 +0900

    Add navigation

---
+++ components/Navigation.vue
@@ -0,0 +1,18 @@
+<template>
+  <d-navbar toggleable="md" type="dark" theme="primary">
+    <d-navbar-toggle target="nav-collapse"></d-navbar-toggle>
+    <d-navbar-brand>
+      <h6 class="text-white my-auto mr-4">MissionDay高岡</h6>
+    </d-navbar-brand>
+
+    <d-collapse is-nav id="nav-collapse">
+      <d-navbar-nav>
+        <d-nav-item to="/">ホーム</d-nav-item>
+        <d-nav-item to="/mission">ミッション</d-nav-item>
+        <d-nav-item to="/spot">おすすめスポット</d-nav-item>
+        <d-nav-item to="/help">ヘルプ</d-nav-item>
+      </d-navbar-nav>
+    </d-collapse>
+  </d-navbar>
+</template>
+​
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
+    <Navigation />
     <nuxt />
   </div>
 </template>
+
+<script>
+import Navigation from '~/components/Navigation.vue'
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
 
 <style>
 html {
