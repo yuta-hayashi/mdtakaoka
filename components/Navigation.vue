a4dcf339fb8ea119c04afe2a3cb136ef4e8d3db0
yuta-hayashi <dev.hyuta@gmail.com>
2019-07-06 22:42:16 +0900

    titleの追加とnavigationの修正

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,48 +1,108 @@
 <template>
   <div id="navigation">
+    <h1 id="title">MissionDay 高岡</h1>
     <div id="navigations">
-      <nuxt-link to="/">
-        <img src="~/assets/icon/home.svg" /><span>ホーム</span>
+      <nuxt-link to="/" class="navBotton home" exact-active-class="active">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <path fill="none" d="M0 0h24v24H0V0z" />
+          <path
+            d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1z"
+          />
+        </svg>
+        <span>ホーム</span>
       </nuxt-link>
-      <nuxt-link to="/mission">
-        <img src="~/assets/icon/map.svg" /><span>ミッション</span>
+      <nuxt-link to="/mission" class="navBotton mission" active-class="active">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <path fill="none" d="M0 0h24v24H0V0z" />
+          <path
+            d="M14.65 4.98l-5-1.75c-.42-.15-.88-.15-1.3-.01L4.36 4.56C3.55 4.84 3 5.6 3 6.46v11.85c0 1.41 1.41 2.37 2.72 1.86l2.93-1.14c.22-.09.47-.09.69-.01l5 1.75c.42.15.88.15 1.3.01l3.99-1.34c.81-.27 1.36-1.04 1.36-1.9V5.69c0-1.41-1.41-2.37-2.72-1.86l-2.93 1.14c-.22.08-.46.09-.69.01zM15 18.89l-6-2.11V5.11l6 2.11v11.67z"
+          />
+        </svg>
+        <span>ミッション</span>
       </nuxt-link>
-      <nuxt-link to="/spot">
-        <img src="~/assets/icon/star.svg" /><span>おすすめ</span>
+      <nuxt-link to="/spot" class="navBotton spot" active-class="active">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <path fill="none" d="M0 0h24v24H0V0z" />
+          <path
+            d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zm3.23 15.39L12 15.45l-3.22 1.94c-.38.23-.85-.11-.75-.54l.85-3.66-2.83-2.45c-.33-.29-.15-.84.29-.88l3.74-.32 1.46-3.45c.17-.41.75-.41.92 0l1.46 3.44 3.74.32c.44.04.62.59.28.88l-2.83 2.45.85 3.67c.1.43-.36.77-.74.54z"
+          />
+        </svg>
+        <span>おすすめ</span>
       </nuxt-link>
-      <nuxt-link to="/help">
-        <img src="~/assets/icon/help.svg" /><span>ヘルプ</span>
+      <nuxt-link to="/help" class="navBotton help" active-class="active">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <path fill="none" d="M0 0h24v24H0V0z" />
+          <path
+            d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92c-.5.51-.86.97-1.04 1.69-.08.32-.13.68-.13 1.14h-2v-.5c0-.46.08-.9.22-1.31.2-.58.53-1.1.95-1.52l1.24-1.26c.46-.44.68-1.1.55-1.8-.13-.72-.69-1.33-1.39-1.53-1.11-.31-2.14.32-2.47 1.27-.12.37-.43.65-.82.65h-.3C8.4 9 8 8.44 8.16 7.88c.43-1.47 1.68-2.59 3.23-2.83 1.52-.24 2.97.55 3.87 1.8 1.18 1.63.83 3.38-.19 4.4z"
+          />
+        </svg>
+        <span>ヘルプ</span>
       </nuxt-link>
     </div>
   </div>
 </template>
 ​
 <style>
+#title {
+  font-size: 2em;
+  margin: 0;
+}
 #navigation {
-  background-color: gray;
+  background-color: aliceblue;
   padding: 1em;
   position: fixed;
   margin: 0 0 0 auto;
   z-index: 1;
   width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
-#navigations{
-  background-color: red;
-  text-align: right;
+#navigations {
+  padding-top: 1em;
 }
-
+.navBotton {
+  padding: 10px;
+  color: gray;
+  fill: gray;
+}
+.active {
+  background-color: #17c671;
+  fill: white;
+}
+.active span {
+  color: white;
+}
+span {
+  padding: 5px;
+}
+a:hover{
+  text-decoration: none;
+}
 @media (max-width: 400px) {
+  #title {
+    display: none;
+  }
   #navigation {
+    display: block;
     position: fixed;
     bottom: 0;
+    padding: 0;
+    flex: none;
   }
-  #navigations{
+  .navBotton {
+    width: 25%;
+  }
+  #navigations {
     display: flex;
-    justify-content: space-between;
     text-align: center;
+    padding: 0;
   }
-  span{
+  span {
+    padding: 0;
+    font-size: 0.8em;
     display: block;
+    color: black;
   }
 }
 </style>
