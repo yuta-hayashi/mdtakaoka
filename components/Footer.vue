66b0952e22e5642c191302b6575532e0e8a84786
yuta-hayashi <dev.hyuta@gmail.com>
2019-07-06 23:46:32 +0900

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="footer">
+      <p>
+        このサイトはMD高岡の実行を支援する有志によって運営されています。
+        <a href="/policy" class="link">privacy polisy</a>
+      </p>
+      <p>&copy; 2019 mdtakaoka.info</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.link {
+  color: gray;
+}
+.footer p {
+  margin: 0;
+}
+.footer {
+  padding: 10px;
+  text-align: center;
+}
+@media (max-width: 400px) {
+  .footer {
+    padding-bottom: 4em;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,17 @@
   <div>
     <Navigation />
     <nuxt />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navigation from "~/components/Navigation.vue";
+import Footer from "~/components/Footer.vue";
 export default {
   components: {
-    Navigation
+    Navigation,
+    Footer,
   }
 };
 </script>
