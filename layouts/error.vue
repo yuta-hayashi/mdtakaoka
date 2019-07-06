f5ff594468eea6b55af61f20dbb62dfe48fd776d
yuta-hayashi <dev.hyuta@gmail.com>
2019-07-06 11:22:40 +0900

    change error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>Error Page</h1>
+  <div class="container">
+    <h1>ページが見つかりません。</h1>
+    <nuxt-link to="/">ホームへ戻る</nuxt-link>
+    <br>
+    <img src="~/assets/img/empty.svg" id="empty">
+  </div>
 </template>
+
+<style>
+.container{
+    text-align: center;
+}
+#empty{
+    width: 80%;
+}
+</style>
