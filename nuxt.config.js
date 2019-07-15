e901e86cdc8c3c7a457fcf4768aa4ff318a8074f
yuta-hayashi <dev.hyuta@gmail.com>
2019-07-15 11:58:17 +0900

    config netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,5 +63,10 @@ export default {
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
     CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
     CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID
+  },
+  generate:{
+    routes:[
+      '/error'
+    ]
   }
 }
