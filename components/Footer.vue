35c11fe21f64aea4c4be8fd93b9696290277af2d
yuta-hayashi <dev.hyuta@gmail.com>
2019-07-19 10:35:25 +0900

    表記の変更

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="footer">
     <p>
-      このサイトはMD高岡の実行を支援する有志によって運営されています。
+      制作：MissionDay高岡 実行委員会, 
       <a href="/policy" class="link">privacy polisy</a>
     </p>
     <p>&copy; 2019 mdtakaoka.info</p>
@@ -14,12 +14,12 @@
 }
 .footer p {
   margin: 0;
-  background-color: #f3f3f3;
 }
 .footer {
   margin-top: 3em;
   padding: 10px;
   text-align: center;
+  background-color: #f3f3f3;
 }
 @media (max-width: 720px) {
   .footer {
