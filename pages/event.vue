a515d8ef0bfcb688bb1d99126304ea560c88e28e
yuta-hayashi <dev.hyuta@gmail.com>
2019-08-11 00:13:28 +0900

    イベントページの追加

---
+++ pages/event.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="content">
+    <div id="top-text">
+      <h2>イベント情報</h2>
+      <p>MissionDay高岡で開催されるイベント情報です。</p>
+    </div>
+    <div class="card-list">
+      <d-card v-for="item in posts" :key="item.sys.id" class="card">
+        <d-card-img :src="item.fields.cover.fields.file.url + '?fit=thumb&f=top&h=230&w=400'" top />
+        <d-card-body :title=item.fields.title>
+          <div v-html="item.fields.content" class="post"></div>
+        </d-card-body>
+      </d-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import { createClient } from "~/plugins/contentful.js";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
+
+const client = createClient();
+const options = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: ({
+      data: {
+        target: { fields }
+      }
+    }) =>
+      `<img src="${fields.file.url}" alt="${fields.description}" class="post-img"/>`
+  }
+};
+
+export default {
+  async asyncData({ env }) {
+    return Promise.all([
+      // fetch all blog posts sorted by creation date
+      client.getEntries({
+        content_type: "event",
+        order: "-sys.createdAt"
+      })
+    ])
+      .then(([posts]) => {
+        // return data that should be available
+        // in the template
+        console.log(posts.items[0].fields.content);
+        posts.items.map(function(value) {
+          value.fields.content = documentToHtmlString(
+            value.fields.content,
+            options
+          );
+        });
+        return {
+          posts: posts.items
+        };
+      })
+      .catch(console.error);
+  }
+};
+</script>
+
+
+<style>
+h2 {
+  color: gray;
+}
+#top-text {
+  margin: 5%;
+}
+.card {
+  margin: 30px;
+  min-width: 300px;
+  max-width: 500px;
+}
+.card-title {
+  font-weight: bold;
+}
+.post-img{
+  width: 100%;
+}
+</style>
