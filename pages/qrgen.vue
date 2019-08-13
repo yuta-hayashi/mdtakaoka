88bf302cfb35be19b20dd4c7d2764fe27f235fec
yuta-hayashi <dev.hyuta@gmail.com>
2019-08-13 22:35:04 +0900

    QR code生成機能を追加

--- pages/qrgen.vue
+++ pages/qrgen.vue
@@ -1,38 +1,58 @@
 <template>
-  <div class="content">
-    <h1>QR-生成</h1>
-    <p>AgentNameを入力してください</p>
-    <input v-model="inputText" placeholder="AgentName" />
-    <input type="button" @click="generate" value="生成" />
-    <qrcode-vue :value="name" :size="size" level="H" class="qrimg"></qrcode-vue>
-  </div>
+  <no-ssr>
+    <div>
+      <h1>QR-生成</h1>
+      <p>AgentNameを入力してください</p>
+      <input v-model="inputText" placeholder="AgentName" />
+      <input type="button" @click="generate" value="生成" />
+      <h4>{{name}}</h4>
+      <QRCanvas v-if="status" :options="options" @updated="onUpdated" />
+    </div>
+  </no-ssr>
 </template>
+
 <script>
-import QrcodeVue from "qrcode.vue";
+import { QRCanvas } from "qrcanvas-vue";
 
 export default {
+  components: {
+    QRCanvas
+  },
   data() {
     return {
-      name: "",
-      size: 300
+      options: {
+        cellSize: 8,
+        correctLevel: "H",
+        data: ""
+      },
+      name:"",
+      inputText: "",
+      status: false
     };
   },
-  methods:{
-      generate:function(){
-          this.name=this.inputText;
-      }
+  methods: {
+    onUpdated() {
+      console.log("updated");
+    },
+    generate(){
+      console.log(this.options.data)
+      this.options = Object.assign({}, this.options, {
+        data: this.inputText
+      });
+      this.name=this.inputText;
+      this.status = true;
+    }
   },
-  components: {
-    QrcodeVue
+  mounted() {
+    const image = new Image();
+    image.src = "/qr-icon.png";
+    image.onload = () => {
+      this.options = Object.assign({}, this.options, {
+        logo: {
+          image
+        }
+      });
+    };
   }
 };
 </script>
-
-<style scoped>
-.content {
-  text-align: center;
-}
-.qrimg {
-  padding: 2em;
-}
-</style>
