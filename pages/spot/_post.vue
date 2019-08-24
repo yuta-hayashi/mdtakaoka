fb5b60f9626f8904ece7df1ed8e531c7c8134a09
yuta-hayashi <dev.hyuta@gmail.com>
2019-08-25 01:19:57 +0900

    記事のレイアウトを修正

--- pages/spot/_post.vue
+++ pages/spot/_post.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="content">
-    <h1 class="title">{{title}}</h1>
-    <div v-html="post" class="post"></div>
+  <div class="post-content">
+    <div class="content">
+      <img :src="cover+'?fm=jpg&q=50'" class="post-img cover-img" />
+      <h2 class="title">{{title}}</h2>
+      <div v-html="post" class="post"></div>
+    </div>
   </div>
 </template>
 
@@ -17,8 +20,9 @@ const options = {
       `<a href=${
         node.data.uri
       } target="_blank" rel="noreferrer noopener">${next(node.content)}</a>`,
-       [BLOCKS.EMBEDDED_ASSET]: (node) =>
-      `<img src="${node.data.target.fields.file.url+'?fm=jpg&q=50'}" class="post-img"/>`
+    [BLOCKS.EMBEDDED_ASSET]: node =>
+      `<img src="${node.data.target.fields.file.url +
+        "?fm=jpg&q=80"}" class="post-img shadow"/>`
   }
 };
 
@@ -36,6 +40,7 @@ export default {
       .then(entrie => {
         //console.log(entrie.fields.article);
         return {
+          cover: entrie.fields.cover.fields.file.url,
           title: entrie.fields.title,
           post: documentToHtmlString(entrie.fields.article, options)
         };
@@ -44,19 +49,42 @@ export default {
   },
   head() {
     return { title: this.title + " | " };
+  },
+  transitions: {
+    name: "page",
+    mode: "out-in"
   }
 };
 </script>
 
 <style scoped>
+.content {
+  width: 60%;
+}
+.post-content {
+  display: flex;
+  justify-content: center;
+}
 .title {
   padding: 10px;
+  font-weight: bold;
 }
 .post {
   margin: 0 5%;
 }
-.post-img {
-  width: 80%;
-  max-width: 800px;
+.shadow {
+  box-shadow: 0 0.46875rem 2.1875rem rgba(90, 97, 105, 0.1),
+    0 0.9375rem 1.40625rem rgba(90, 97, 105, 0.1),
+    0 0.25rem 0.53125rem rgba(90, 97, 105, 0.12),
+    0 0.125rem 0.1875rem rgba(90, 97, 105, 0.1);
+}
+.cover-img {
+  border-bottom-left-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+}
+@media (max-width: 875px) {
+  .content {
+    width: 100%;
+  }
 }
 </style>
